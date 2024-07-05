cf8b2e3d8b7d8811d00f3ccfaed02fc8a2a4f8d2
Kuba Malinowski <210psc@gmail.com>
2024-07-05 17:06:05 +0200

    Changed bomb icon, added "Bombinfo".

--- src/board.ts
+++ src/board.ts
@@ -1,43 +1,72 @@
-import arrayShuffle from 'array-shuffle';
+import arrayShuffle from "array-shuffle";
 
 enum State {
-    Bomb = 'B',
-    Empty = ''
+  Bomb = "💣",
+  Empty = "",
+  One = "1",
+  Two = "2",
+  Three = "3",
+  Four = "4",
+  Five = "5",
+  Six = "6",
+  Seven = "7",
+  Eight = "8",
 }
 
 export class Board {
-    private width: number;
-    private height: number;
-    private numBombs: number;
-    private state: State[];
+  private width: number;
+  private height: number;
+  private numBombs: number;
+  private state: State[];
 
-    constructor(width: number, height: number, numBombs: number) {
-        this.width = width;
-        this.height = height;
-        this.numBombs = numBombs;
-        
-        this.state = [];
-        for (let i = 0; i < this.height; i++) {
-            for (let j = 0; j < this.width; j++) {
-                this.state.push(State.Empty);
-            }
-        }
+  constructor(width: number, height: number, numBombs: number) {
+    this.width = width;
+    this.height = height;
+    this.numBombs = numBombs;
 
-        for (let i = 0; i < this.numBombs; i++) {
-            this.state[i] = State.Bomb
-        }
-        this.state = arrayShuffle(this.state)
+    // state
+    this.state = Array(this.width * this.height).fill(State.Empty);
+
+    // bombs
+    for (let i = 0; i < this.numBombs; i++) {
+      this.state[i] = State.Bomb;
+    }
+    this.state = arrayShuffle(this.state);
+
+    // bomb info
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        const state = this.getXYState(x, y);
+        if (state === State.Bomb) continue;
+        let numBombs = 0;
+        if (this.getXYState(x - 1, y - 1) === State.Bomb) numBombs++;
+        if (this.getXYState(x, y - 1) === State.Bomb) numBombs++;
+        if (this.getXYState(x + 1, y - 1) === State.Bomb) numBombs++;
+        if (this.getXYState(x - 1, y) === State.Bomb) numBombs++;
+        if (this.getXYState(x + 1, y) === State.Bomb) numBombs++;
+        if (this.getXYState(x - 1, y + 1) === State.Bomb) numBombs++;
+        if (this.getXYState(x, y + 1) === State.Bomb) numBombs++;
+        if (this.getXYState(x + 1, y + 1) === State.Bomb) numBombs++;
+        if (numBombs > 0)
+        //@ts-ignore
+        this.state[y * this.width + x] = numBombs.toString();
+      }
+    }
+  }
+
+  getXYState(x: number, y: number): State {
+    if (x < 0 || x >= this.width || y < 0 || y >= this.height) {
+      return State.Empty;
     }
 
-    getXYState(x: number, y: number): State {
-        return this.state[y*this.width+x];
-    }
+    return this.state[y * this.width + x];
+  }
 
-    getWidth(): number {
-        return this.width;
-    }
+  getWidth(): number {
+    return this.width;
+  }
 
-    getHeight(): number {
-        return this.height;
-    }
+  getHeight(): number {
+    return this.height;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,21 @@
-import './style.css';
+import "./style.css";
 import { Board } from "./board.ts";
-
 
 function initGame(container: HTMLDivElement | null) {
   if (!container) {
-    throw new Error('Error loading game');
+    throw new Error("Error loading game");
   }
 
-  const board = new Board(7, 3, 5);
+  const board = new Board(13, 7, 20);
   const width = board.getWidth();
   const height = board.getHeight();
 
-  const table = document.createElement('table');
+  const table = document.createElement("table");
 
   for (let y = 0; y < height; y++) {
-    const row = document.createElement('tr');
+    const row = document.createElement("tr");
     for (let x = 0; x < width; x++) {
-      const cell = document.createElement('td');
+      const cell = document.createElement("td");
       cell.textContent = board.getXYState(x, y);
       row.appendChild(cell);
     }
@@ -26,5 +25,5 @@ function initGame(container: HTMLDivElement | null) {
   container.appendChild(table);
 }
 
-const app = document.querySelector<HTMLDivElement>('#app');
+const app = document.querySelector<HTMLDivElement>("#app");
 initGame(app);
