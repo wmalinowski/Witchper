eca6f65199710e71df21f4a83a9d6587bc7ddd62
Kuba Malinowski <210psc@gmail.com>
2024-07-08 19:37:05 +0200

    Added background music and sound effects.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
+import { Howl } from "howler";
+import type { HowlOptions } from "howler";
 import "./style.css";
 import { Board } from "./board.ts";
+import json from "./sprites.json";
+
+function isHowlOptions(json: unknown): json is HowlOptions {
+  return (
+    !!json &&
+    typeof json === "object" &&
+    "src" in json &&
+    Array.isArray(json.src) &&
+    "sprite" in json &&
+    typeof json.sprite === "object"
+  );
+}
+if (!isHowlOptions(json)) throw new Error("Internal error");
+const soundSprites = new Howl(json);
 
 function initGame(container: HTMLDivElement | null) {
   if (!container) {
@@ -18,7 +34,16 @@ function initGame(container: HTMLDivElement | null) {
       const cell = document.createElement("td");
       cell.classList.add("covered");
       cell.onclick = () => {
-        cell.textContent = board.getXYState(x, y);
+        const cellState = board.getXYState(x, y);
+        switch (cellState) {
+          case "💣":
+            soundSprites.play("monster");
+            break;
+          case "":
+            soundSprites.play("clearing-fog");
+            break;
+        }
+        cell.textContent = cellState;
         cell.classList.remove("covered");
         cell.classList.add("uncovered");
       };
@@ -30,5 +55,26 @@ function initGame(container: HTMLDivElement | null) {
   container.appendChild(table);
 }
 
+function switchMusic(event: Event) {
+  const switchEl = <HTMLSelectElement>event.currentTarget;
+  if (!switchEl.value) {
+    return;
+  }
+
+  const value = switchEl.value;
+  soundSprites.stop();
+  switch (value) {
+    case "background1":
+      soundSprites.play("background1");
+      break;
+    case "background2":
+      soundSprites.play("background2");
+      break;
+  }
+}
+
 const app = document.querySelector<HTMLDivElement>("#app");
 initGame(app);
+
+const musicSwitcher = document.querySelector<HTMLDivElement>("#music-switcher");
+musicSwitcher?.addEventListener("change", switchMusic);
